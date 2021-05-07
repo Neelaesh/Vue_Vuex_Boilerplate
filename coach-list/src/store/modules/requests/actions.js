ce16bfd319a097e223501129a2c3c48b1871e2dd
Neelaesh <neelaesh006@gmail.com>
2021-05-07 16:53:35 +0530

    Patch Updates v 2.0.3

--- coach-list/src/store/modules/requests/actions.js
+++ coach-list/src/store/modules/requests/actions.js
@@ -1,11 +1,34 @@
 export default {
-    addRequest(context, data) {
+    async addRequest(context, payload) {
         let newRequest = {
-            id: new Date().toISOString(),
-            coachId: data.coachId,
-            email: data.email,
-            message: data.message
+            email: payload.email,
+            message: payload.message
         };
+        const response = await fetch(`https://vue-http-demo-906e8-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        });
+        const responseData = await response.json();
+        if(!response.ok) {
+            const error = new Error(responseData.message || 'Failed to Fetch');
+            throw error;
+        }
+        newRequest['coachId'] = payload.coachId;
         context.commit('addNewRequest', newRequest);
+    },
+    async fetchRequests(context) {
+        const coachId = context.rootGetters.userId;
+        const response = await fetch(`https://vue-http-demo-906e8-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+        const responseData = await response.json();
+        let requests = [];
+        for(let key in responseData) {
+            let request = {
+                id: responseData[key],
+                email: responseData[key]['email'],
+                message: responseData[key]['message']
+            }
+            requests.push(request);
+        }
+        context.commit('setRequests', requests);
     }
 }
--- coach-list/src/store/modules/requests/mutations.js
+++ coach-list/src/store/modules/requests/mutations.js
@@ -1,5 +1,8 @@
 export default {
     addNewRequest(state, payload) {
         state.requests.push(payload);
+    },
+    setRequests(state, payload) {
+        state.requests = payload;
     } 
 }
