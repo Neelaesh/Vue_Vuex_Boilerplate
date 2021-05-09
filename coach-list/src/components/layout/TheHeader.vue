0a9e78ad305e10eefef2d9d812ac4edac3341bff
Neelaesh <neelaesh006@gmail.com>
2021-05-09 20:14:57 +0530

    Patch Updates v 3.0.2

--- coach-list/src/components/layout/TheHeader.vue
+++ coach-list/src/components/layout/TheHeader.vue
@@ -1,13 +1,13 @@
 <template>
-    <header>
-        <nav>
-        <h1><router-link to="/">Find a Coach</router-link></h1>
-        <ul>
-            <li><router-link to="/coaches">Coaches</router-link></li>
-            <li><router-link to="/requests">Requests</router-link></li>
-        </ul>
-        </nav>
-    </header>
+  <header>
+    <nav>
+      <h1><router-link to="/">Find a Coach</router-link></h1>
+      <ul>
+        <li><router-link to="/coaches">Coaches</router-link></li>
+        <li><router-link to="/requests">Requests</router-link></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <style scoped>
